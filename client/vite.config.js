9addcab5d85903de72fa70a9faa443ea60d0f1b3
THURAKA SREERAM <146184197+SREERAM9652@users.noreply.github.com>
2025-10-26 01:39:35 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -15,7 +15,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: process.env.VITE_API_BASE_URL || 'https://quadsync-events.onrender.com', // ✅ fallback
+        target: process.env.VITE_API_BASE_URL || 'https://quad-sync-events-yh13.vercel.app', // ✅ fallback
         changeOrigin: true,
         secure: false,
       },
